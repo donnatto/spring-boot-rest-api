2525a597794d752accdbf8ad409afc25d25f0812
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-02 23:37:38 -0500

    add postgres, jdbc and flyway dependencies. add person table sql

--- src/main/java/me/donnatto/restapi/model/Person.java
+++ src/main/java/me/donnatto/restapi/model/Person.java
@@ -2,10 +2,12 @@ package me.donnatto.restapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 public class Person {
     private final UUID uuid;
+    @NotBlank
     private final String name;
 
     public Person(@JsonProperty("id") UUID uuid,
