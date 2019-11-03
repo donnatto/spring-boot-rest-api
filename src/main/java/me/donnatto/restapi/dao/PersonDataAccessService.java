c5c05b709a5ad5d71c4ac1a25e15b837becd3493
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-03 00:08:49 -0500

    add postgresql integration

--- src/main/java/me/donnatto/restapi/dao/PersonDataAccessService.java
+++ src/main/java/me/donnatto/restapi/dao/PersonDataAccessService.java
@@ -1,12 +1,22 @@
 package me.donnatto.restapi.dao;
 
 import me.donnatto.restapi.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -14,7 +24,12 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return Collections.singletonList(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
