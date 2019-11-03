293e3ee9269713026b581fd6b87206089301d44b
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-02 22:20:58 -0500

    add update and delete methods

--- src/main/java/me/donnatto/restapi/dao/FakePersonDattaAccessService.java
+++ src/main/java/me/donnatto/restapi/dao/FakePersonDattaAccessService.java
@@ -33,12 +33,24 @@ public class FakePersonDattaAccessService implements PersonDao {
 
     @Override
     public int deletePersonById(UUID id) {
-        return 0;
+        Optional<Person> personMaybe = selectPersonById(id);
+        if (!personMaybe.isPresent()) {
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
     }
 
     @Override
-    public int updatePersonById(UUID id, Person person) {
-        return 0;
+    public int updatePersonById(UUID id, Person update) {
+        return selectPersonById(id)
+                .map(p -> {
+                    int indexOfPersonToUpdate = DB.indexOf(p);
+                    if (indexOfPersonToUpdate >= 0) {
+                        DB.set(indexOfPersonToUpdate, new Person(id, update.getName()));
+                        return 1;
+                    } return 0;
+                }).orElse(0);
     }
 
 }
